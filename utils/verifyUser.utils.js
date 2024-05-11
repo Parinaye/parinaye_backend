64920bed952caa1ea645efd99b0a843856fd02cc
AmarnathN <vayuteja@gmail.com>
2024-05-11 17:35:53 +0530

    update verifytoken to accept auth token only

--- utils/verifyUser.utils.js
+++ utils/verifyUser.utils.js
@@ -2,23 +2,21 @@ import { errorHandler } from "./error.utils.js";
 import jwt from "jsonwebtoken";
 
 export const verifyToken = async (req, res, next) => {
-  let token = req.cookies.access_token;
-  if (!token)
-    if (req.headers.authorization) {
-      token = req.headers.authorization.split(" ")[1];
-    } else {
-      return res
-        .status(401)
-        .json({ success: false, message: "Access denied !!, provide token." });
-    }
+  let token;
+  // let token = req.cookies.access_token;
+  // if (!token)
+  if (req.headers.authorization) {
+    token = req.headers.authorization.split(" ")[1];
+  } else {
+    return res
+      .status(401)
+      .json({ success: false, message: "Access denied !!, provide Authorization." });
+  }
   try {
     console.log(token);
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       console.log(err);
-      if (err)
-        return next(
-          errorHandler(403, "Please Re-Authenticate. !! ")
-        );
+      if (err) return next(errorHandler(403, "Please Re-Authenticate. !! "));
       req.user = decoded;
       next();
     });
@@ -31,15 +29,16 @@ export const verifyToken = async (req, res, next) => {
 };
 
 export const verifyResetPasswordToken = async (req, res, next) => {
-  let token = req.cookies.reset_password_token;
-  if (!token)
-    if (req.headers.authorization) {
-      token = req.headers.authorization.split(" ")[1];
-    } else {
-      return res
-        .status(401)
-        .json({ success: false, message: "Access denied !!, provide token." });
-    }
+  // let token = req.cookies.reset_password_token;
+  // if (!token)
+  let token;
+  if (req.headers.authorization) {
+    token = req.headers.authorization.split(" ")[1];
+  } else {
+    return res
+      .status(401)
+      .json({ success: false, message: "Access denied !!, provide Authorization." });
+  }
   try {
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       console.log(err);
